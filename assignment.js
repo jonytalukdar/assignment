27a707e03c1dbb16ee97d1e402ec7d52c1798761
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-21 23:25:44 +0600

    complete one and two problem

---
+++ assignment.js
@@ -0,0 +1,24 @@
+// first problem
+
+function kilometerToMeter(kilometer) {
+  var meter = kilometer * 1000;
+  return meter;
+}
+
+var resultOfMeter = kilometerToMeter(12);
+console.log(resultOfMeter);
+
+// second problem
+
+function budgetCalculator(ghori, phone, laptop) {
+  var totalGhori = 4 * ghori;
+  var totalPhone = 3 * phone;
+  var totalLaptop = 2 * laptop;
+  totatBudget = totalGhori + totalPhone + totalLaptop;
+  return totatBudget;
+}
+
+var resultOfBudget = budgetCalculator(50, 100, 500);
+console.log(resultOfBudget);
+
+// third problem
